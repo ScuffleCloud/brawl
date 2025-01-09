b564dcb9619d6e9538d3a35fdcb6e07e097f0312
Troy Benson <troybensonsa@gmail.com>
2025-01-09 20:32:56 +0000

    fix lint
    cleanup a missed lint from a previous pr

--- server/src/command/dry_run.rs
+++ server/src/command/dry_run.rs
@@ -920,7 +920,7 @@ mod tests {
 
         let pr = PullRequest {
             number: 1,
-            merged_at: Some(Utc::now().into()),
+            merged_at: Some(Utc::now()),
             ..Default::default()
         };
 
